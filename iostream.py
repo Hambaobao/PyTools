4534dd31d2a12bd4554796f8d67bb25edf2e2753
Hambaobao <jameszhang2880@gmail.com>
2021-12-13 21:41:05 +0800

    add 2 more functions

--- iostream.py
+++ iostream.py
@@ -7,11 +7,42 @@ def save_per_line(filepath, data_list):
         Parameters
         -----------------------------------------
         filepath: str
-            the path to save the file, eg. "./workspace/file.txt"
+            the path to save the file, eg. "./workspace/file.txt".
         data_list: list
             list of strings, where strings are to be written into 
-            file at file_path one per line
+            file at file_path one per line.
     '''
 
     with Path(filepath).open("w") as f:
         f.writelines([f"{line}\n" for line in data_list])
+
+
+def read_per_line(filepath):
+    ''' read text from file form of list of string,
+        each element of list is a string object.
+
+        Parameters
+        ------------------------------
+        filepath: str
+            the path of file that one will read from.
+    '''
+    with open(filepath) as f:
+        lines = f.readlines()
+        lines = [l.strip('\n') for l in lines]
+
+    return lines
+
+
+def write_text2file(path: str, text: int):
+    ''' write string to file
+    
+        Parameters
+        -------------------------
+        path: str
+            the path to save the file, eg. "./workspace/file.txt".
+        text: str
+            the content will be write to file.
+    '''
+    file = Path(path)
+    file.parent.mkdir(exist_ok=True, parents=True)
+    file.write_text(text)
